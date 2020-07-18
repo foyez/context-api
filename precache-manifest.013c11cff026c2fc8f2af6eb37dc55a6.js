cf2444661dec43e3797435659752b794171d40d8
foyez <foyezar@yahoo.com>
2020-07-18 17:14:48 +0600

    Updates

---
+++ precache-manifest.013c11cff026c2fc8f2af6eb37dc55a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "288d6481aa2e5d1f022593f1a8657ead",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6cad56feee24d7f3b61",
+    "url": "/static/css/main.bccd0344.chunk.css"
+  },
+  {
+    "revision": "ec8022b117ea73aad6c4",
+    "url": "/static/js/2.2627f34c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2627f34c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6cad56feee24d7f3b61",
+    "url": "/static/js/main.d48d977d.chunk.js"
+  },
+  {
+    "revision": "a1d6a4fc6826fc8bf9ba",
+    "url": "/static/js/runtime-main.008f8fb2.js"
+  }
+]);
